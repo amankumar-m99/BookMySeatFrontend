d6231a7827cd460b9a9d0eb3747bf73909306828
Aman Kumar <amankumar.m99@gmail.com>
2025-01-04 05:49:45 +0530

    added login auth

--- src/app/guards/login/login.guard.ts
+++ src/app/guards/login/login.guard.ts
@@ -8,15 +8,20 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 
 export class LoginGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      if(this.authService.isAuthenticated()){
-        return false;
+    if (this.authService.isAuthenticated()) {
+      switch (localStorage.getItem("role")) {
+        case "superadmin": this.router.navigate(["/dashboard/super-admin"]); break;
+        case "theateradmin": this.router.navigate(["/dashboard/theater-admin"]); break;
+        default: this.router.navigate(["/dashboard/user"]); break;
       }
+      return false;
+    }
     return true;
   }
-  
+
 }
