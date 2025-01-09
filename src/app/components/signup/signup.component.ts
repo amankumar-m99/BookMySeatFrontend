6672641df7bb5e87b6f68ed0f23a2eab7a9830d3
Aman Kumar <amankumar.m99@gmail.com>
2025-01-09 19:07:08 +0530

    beautification

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -18,12 +18,13 @@ export class SignUpComponent {
   ) {
     this.signUpForm = this.formBuilder.group({
       email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
       firstName: ['', Validators.required],
       middleName: ['', Validators.required],
       lastName: ['', Validators.required],
-      phoneNumber: ['', Validators.required]
+      isTheaterAdmin: [false, Validators.required]
     });
   }
 
@@ -35,6 +36,7 @@ export class SignUpComponent {
     let middleName: string = this.signUpForm.get("middleName")?.value;
     let lastName: string = this.signUpForm.get("lastName")?.value;
     let phoneNumber: string = this.signUpForm.get("phoneNumber")?.value;
+    let role: string = (this.signUpForm.get("isTheaterAdmin")?.value)?"theateradmin":"user";
     if (this.signUpForm.invalid) {
       alert("Invalid form!");
       return;
@@ -43,7 +45,7 @@ export class SignUpComponent {
       alert("Password and confirm password not matching!");
       return;
     }
-    let signUpFormModel: SignUpFormModel = new SignUpFormModel(email, password, firstName, middleName, lastName, phoneNumber);
+    let signUpFormModel: SignUpFormModel = new SignUpFormModel(email, password, firstName, middleName, lastName, phoneNumber, role);
     this.signUpService.signUp(signUpFormModel).subscribe({
       next: (response) => {
         alert("Sign up success.")
