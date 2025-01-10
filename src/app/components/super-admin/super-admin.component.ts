7573c21021f5eaf832e3c48e4bd81cb76e3047ba
Aman Kumar <amankumar.m99@gmail.com>
2025-01-10 17:29:53 +0530

    initial admin console

--- src/app/components/super-admin/super-admin.component.ts
+++ src/app/components/super-admin/super-admin.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MovieService } from 'src/app/services/movie/movie.service';
+import { TheaterService } from 'src/app/services/theater/theater.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-super-admin',
@@ -6,5 +10,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./super-admin.component.css']
 })
 export class SuperAdminComponent {
+
+  totalMovies = 0;
+  totalTheaters = 0;
+  totalAdmins = 0;
+  totalUsers = 0;
+
+  constructor(
+    private movieService: MovieService,
+    private theaterService: TheaterService,
+    private userService: UserService,
+    private router: Router) {
+    this.fetchMovieCount();
+    this.fetchTheaterCount();
+    this.fetchAdminCount();
+    this.fetchUserCount();
+  }
+
+  fetchMovieCount() {
+    this.movieService.getMoviesCount().subscribe({
+      next: (response) => this.totalMovies = response,
+      error: (error) => { }
+    });
+  }
+
+  fetchTheaterCount() {
+    this.theaterService.getTheaterCount().subscribe({
+      next: (response) => this.totalTheaters = response,
+      error: (error) => { }
+    });
+  }
+
+  fetchAdminCount() {
+    this.userService.getTheaterAdminCount().subscribe({
+      next: (response) => this.totalAdmins = response,
+      error: (error) => { }
+    });
+  }
+
+  fetchUserCount() {
+    this.userService.getUserCount().subscribe({
+      next: (response) => this.totalUsers = response,
+      error: (error) => { }
+    });
+  }
+
+  navigate(path: string) {
+    path = "/dashboard/super-admin/" + path;
+    this.router.navigate([ path]);
+  }
 
 }
